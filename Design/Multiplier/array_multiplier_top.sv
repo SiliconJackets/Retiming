015ddb7b5b6d8a7c4a88dbe0cdbe358bd712fa26
jsowmya <jsowmya@gatech.edu>
2025-03-21 11:53:24 -0400

    tetsing top module

--- Design/Multiplier/array_multiplier_top.sv
+++ Design/Multiplier/array_multiplier_top.sv
@@ -1,14 +1,22 @@
 `timescale 1ns / 1ps
  
-module array_multiplier_top;
+module array_multiplier_top(
+  input logic clk,
+  input logic rst,
+  input logic i_valid,
+  input logic [DATAWIDTH-1:0] A,
+  input logic [DATAWIDTH-1:0] B,
+  output logic o_valid,
+  output logic [DATAWIDTH*2-1:0] Z_final
+);
   localparam TESTDATAWIDTH = 16;
 
-  logic [TESTDATAWIDTH-1:0] A, B;  // Test inputs
-  logic [2*TESTDATAWIDTH-1:0] Z;   // Output product
-  logic rst;
-  logic clk;
-  logic i_vld;
-  logic o_vld;
+  // logic [TESTDATAWIDTH-1:0] A, B;  // Test inputs
+  // logic [2*TESTDATAWIDTH-1:0] Z;   // Output product
+  // logic rst;
+  // logic clk;
+  // logic i_vld;
+  // logic o_vld;
  
   // Instantiate the 8-bit array multiplier
   array_multiplier #(
@@ -19,11 +27,12 @@ module array_multiplier_top;
   mul0 (
     .A(A),
     .B(B),
-    .Z_final(Z),
+    .Z_final(Z_final),
     .clk(clk),
     .rst(rst),
-    .i_valid(i_vld),
-    .o_valid(o_vld)
+    .i_valid(i_valid),
+    .o_valid(o_valid)
   );
  
+
 endmodule
